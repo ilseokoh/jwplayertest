58205457d08581c0d567257b38a4855dd536d92d
오일석 <onestone@live.co.kr>
2017-04-05 18:05:55 +0900

    log html changed to select

--- js/console.js
+++ js/console.js
@@ -3,12 +3,10 @@
         console = {};
     }
     var old = console.log;
-    var logger = document.getElementById('log');
     console.log = function (message) {
-        if (typeof message == 'object') {
-            logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : String(message)) + '<br />';
-        } else {
-            logger.innerHTML += message + '<br />';
-        }
+        $('#log').append($('<option>', {
+            value: 1,
+            text: message
+        }));
     }
 })();
